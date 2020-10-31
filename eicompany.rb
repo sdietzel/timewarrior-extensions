b383dc58b63a834910822ca54c5fb1ee4344bc57
Stefan Dietzel <mail@dietzel.st>
2020-10-31 14:21:06 +0100

    Improvements

--- eicompany.rb
+++ eicompany.rb
@@ -8,6 +8,8 @@ require 'colorize'
 
 HOURS_PER_DAY = 8
 
+SPECIAL_TAGS = ['U', 'K']
+
 require 'holidays/core_extensions/date'
 class Date
   include Holidays::CoreExtensions::Date
@@ -24,8 +26,17 @@ end
 report = ARGF.read
 config, entries = report.split("\n\n")
 
-report_start = DateTime.parse(config.match(/temp.report.start: (?<date>.+)/)[:date]).to_date + 1
-report_end = DateTime.parse(config.match(/temp.report.end: (?<date>.+)/)[:date]).to_date
+report_start = if m = config.match(/temp.report.start: (?<date>.+)/)
+  DateTime.parse(m[:date]).to_time.getlocal.to_date
+else
+  Date.today
+end
+
+report_end = if m = config.match(/temp.report.end: (?<date>.+)/)
+  DateTime.parse(m[:date]).to_time.getlocal.to_date
+else
+  Date.today
+end
 
 entries = JSON.parse(entries)
 
@@ -33,7 +44,7 @@ entries.map! do |e|
   e['start'] = DateTime.parse(e['start'])
   e['end'] = DateTime.parse(e['end'])
   
-  e['tags'].map! { |t| t.upcase }
+  e['tags'].map! { |t| t.upcase } if e.has_key? 'tags'
   
   e['duration'] = (e['end'] - e['start']) * 24.0
   
@@ -44,22 +55,31 @@ tags = Set[]
 days = {}
 
 entries.each do |e|
-  date = e['start'].to_date
+  date = e['start'].to_time.getlocal.to_date
   
   days[date] = {} unless days.has_key? date
 
-  e['tags'].each do |tag|
-    tags.add(tag)
+  unless e.has_key? 'tags'
+    puts "WARNING: Untagged entries on #{date}".red
+  else
+    e['tags'].each do |tag|
+      tags.add(tag)
     
-    days[date][tag] = {'duration' => 0, 'description' => []} unless days[date].has_key? tag
+      days[date][tag] = {'duration' => 0, 'description' => []} unless days[date].has_key? tag
   
-    days[date][tag]['duration'] += e['duration'] / e['tags'].size
-    days[date][tag]['description'] += [e['annotation']] if e.has_key? 'annotation'
+      days[date][tag]['duration'] += e['duration'] / e['tags'].size
+      days[date][tag]['description'] += [e['annotation']] if e.has_key? 'annotation'
+    end
   end
 end
 
 totals = tags.map{ |t| [t, 0] }.to_h
 expected = 0
+
+tags = SPECIAL_TAGS + (tags - Set.new(SPECIAL_TAGS)).to_a
+
+puts '----- ' + report_start.strftime('%^B %Y') + ' -----'
+puts
 
 puts "Date   " + tags.map{ |t| '%10s' % t }.join(' ') + "  Description"
 
@@ -102,9 +122,16 @@ end
 puts
 
 sum = totals.map{ |t, d| d }.reduce(:+)
+productive = totals.map do |t, d|
+  if SPECIAL_TAGS.include? t
+    0
+  else
+    d
+  end
+end.reduce(:+)
 
 puts 'TOTAL  ' + totals.map{ |t, d| '%10.2f' % d }.join(' ')
-puts '       ' + totals.map{ |t, d| '%9.0f%%' % (d / sum * 100) }.join(' ')
+puts '       ' + totals.map{ |t, d| '%9.0f%%' % (d / productive * 100) }.join(' ')
 
 puts
 
