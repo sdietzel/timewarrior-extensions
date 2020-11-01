e38b8560e0f8bfdba9456f794a6c83f9f49414f7
Stefan Dietzel <mail@dietzel.st>
2020-11-01 19:37:44 +0100

    Code style and yellow unknowns

--- eicompany.rb
+++ eicompany.rb
@@ -7,8 +7,8 @@ require 'holidays'
 require 'colorize'
 
 HOURS_PER_DAY = 8
-
-SPECIAL_TAGS = ['U', 'K']
+SPECIAL_TAGS = %w[U K].freeze
+UNKNOWN = '?????'.freeze
 
 require 'holidays/core_extensions/date'
 class Date
@@ -19,7 +19,7 @@ def round_duration(duration)
   (duration * 4).round.to_f / 4.0
 end
 
-def is_workday?(day)
+def workday?(day)
   !(day.holiday?(:de, :de_be) or day.saturday? or day.sunday?)
 end
 
@@ -31,28 +31,28 @@ end
 report = ARGF.read
 config, entries = report.split("\n\n")
 
-report_start = if m = config.match(/temp.report.start: (?<date>.+)/)
-  DateTime.parse(m[:date]).to_time.getlocal.to_date
-else
-  Date.today
-end
+report_start = if (m = config.match(/temp.report.start: (?<date>.+)/))
+                 DateTime.parse(m[:date]).to_time.getlocal.to_date
+               else
+                 Date.today
+               end
 
-report_end = if m = config.match(/temp.report.end: (?<date>.+)/)
-  DateTime.parse(m[:date]).to_time.getlocal.to_date
-else
-  Date.today
-end
+report_end = if (m = config.match(/temp.report.end: (?<date>.+)/))
+               DateTime.parse(m[:date]).to_time.getlocal.to_date
+             else
+               Date.today
+             end
 
 entries = JSON.parse(entries)
 
 entries.map! do |e|
   e['start'] = DateTime.parse(e['start'])
   e['end'] = DateTime.parse(e['end'])
-  
-  e['tags'].map! { |t| t.upcase } if e.has_key? 'tags'
-  
+
+  e['tags'].map!(&:upcase) if e.key? 'tags'
+
   e['duration'] = (e['end'] - e['start']) * 24.0
-  
+
   e
 end
 
@@ -61,68 +61,74 @@ days = {}
 
 entries.each do |e|
   date = e['start'].to_time.getlocal.to_date
-  
-  days[date] = {} unless days.has_key? date
 
-  if e.has_key? 'tags'
-    e['tags'].each do |tag|
-      tags.add(tag)
-    
-      days[date][tag] = {'duration' => 0, 'description' => []} unless days[date].has_key? tag
-  
-      days[date][tag]['duration'] += e['duration'] / e['tags'].size
-      days[date][tag]['description'] += [e['annotation']] if e.has_key? 'annotation'
-    end
+  days[date] = {} unless days.key? date
+
+  e['tags'] = [UNKNOWN] unless e.key? 'tags'
+
+  e['tags'].each do |tag|
+    tags.add(tag)
+
+    days[date][tag] = { 'duration' => 0, 'description' => [] } unless days[date].key? tag
+
+    days[date][tag]['duration'] += e['duration'] / e['tags'].size
+    days[date][tag]['description'] += [e['annotation']] if e.key? 'annotation'
   end
 end
 
 expected = 0
 
 tags = SPECIAL_TAGS + (tags - Set.new(SPECIAL_TAGS)).to_a
-totals = tags.map{ |t| [t, 0] }.to_h
+totals = tags.map { |t| [t, 0] }.to_h
 
-puts "Date     ".underline + ' ' + tags.map{ |t| pad(t, t).underline }.join(' ') + '  ' + 'Description'.underline
+puts 'Date     '.underline + ' ' + tags.map { |t| pad(t, t).underline }.join(' ') + '  ' + 'Description'.underline
 
 (report_start..report_end).each do |day|
-  expected += HOURS_PER_DAY if is_workday? day
-  
-  daystr = is_workday?(day) ? day.strftime('%m/%d %a') : day.strftime('%m/%d %a').light_black
-  
-  unless days.has_key? day
+  expected += HOURS_PER_DAY if workday? day
+
+  daystr = workday?(day) ? day.strftime('%m/%d %a') : day.strftime('%m/%d %a').light_black
+
+  unless days.key? day
     puts daystr
     next
   end
-  
+
   entries = days[day]
-  
+
   str = daystr + ' '
-  
+
   str += tags.map do |t|
-    if entries.has_key? t
+    if entries.key? t
       d = round_duration(entries[t]['duration'])
       totals[t] += d
-      
-      pad(t, '%.2f' % d)
+
+      s = pad(t, '%.2f' % d)
+
+      if t == UNKNOWN
+        s.yellow
+      else
+        s
+      end
     else
       pad(t, '-')
     end
   end.join(' ')
-  
-  str += '  ' + entries.reject{ |k, _| SPECIAL_TAGS.include? k }.map{ |t, e| "#{t}: " + e['description'].join(', ') }.join(', ')
-  
-  unless entries.size > 0
-    str += "WARNING: No tags used!".red
-  end
-  
+
+  str += '  ' + entries.reject { |k, _| SPECIAL_TAGS.include? k }
+                       .map { |t, e| "#{t}: " + e['description'].join(', ') }
+                       .join(', ')
+
+  str += 'WARNING: No tags used!'.red if entries.empty?
+
   puts str
 end
 
-sum = totals.map{ |t, d| d }.reduce(:+)
-productive = totals.reject{ |k, _| SPECIAL_TAGS.include? k }.values.reduce(:+)
+sum = totals.values.reduce(:+)
+productive = totals.reject { |k, _| SPECIAL_TAGS.include? k }.values.reduce(:+)
 
-puts '          ' + totals.map{ |t, d| pad(t, '%.2f' % d) }.join(' ').bold
+puts '          ' + totals.map { |t, d| pad(t, '%.2f' % d) }.join(' ').bold
 puts '          ' + (totals.map do |t, d|
-  unless SPECIAL_TAGS.include? t
+  if !SPECIAL_TAGS.include? t
     pad(t, '%.0f%%' % (d / productive * 100))
   else
     pad(t, '')
